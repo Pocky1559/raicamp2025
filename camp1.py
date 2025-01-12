f086cf023348cadd1c829bccdb0bf4fd9af5557f
Pocky <pissawat2551@gmail.com>
2025-01-12 11:29:42 +0700

    Add camp1.py with fruit list manipulation and mobile robot graph structure

---
+++ camp1.py
@@ -0,0 +1,23 @@
+fruits = ["apple", "banana", "cherry"]
+fruits.append("orange")
+fruits.remove("apple")
+fruits.insert(1, "melon")
+
+# for fruit in fruits :
+#  print(f"This is a {fruit}.")
+
+# i = 0
+# while i < 3 :
+#   print(i)
+#   i += 1
+# print("done")
+
+# Dictionary in Mobile Robot
+graph = {
+  'A': ['B', 'C'],
+  'B': ['D', 'E'],
+  'C': ['F'],
+  'D': [],
+  'E': [],
+  'F': []
+}
